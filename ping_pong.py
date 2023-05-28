d5414d3db8d913579c277b93519a1b0c3680bf07
Makki1337 <134214428+Makki1337@users.noreply.github.com>
2023-05-28 21:13:32 +0300

    Update ping_pong.py

--- ping_pong.py
+++ ping_pong.py
@@ -1,15 +1,14 @@
-#Sidorenko - 1
 from pygame import*
 '''Необходимые классы'''
 
-#класс-родитель для спрайтов
-class GameSprite(sprite.Sprite):
+#класс-родитель для спрайтов  class GameSprite(sprite.Sprite):
     #конструктор класса
         #конструктор класса
+class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, player_speed, width, height):
         super().__init__()
 
-        self.image = transform.scale(image.load(player_image),(widt, height))
+        self.image = transform.scale(image.load(player_image),(width, height))
         self.speed = player_speed
 
         self.rect = self.image.get_rect()
@@ -18,7 +17,7 @@ class GameSprite(sprite.Sprite):
 
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
-#Fedaev - 2
+
 class Player(GameSprite):
     def update_r(self):
         keys = key.get_pressed()
@@ -30,10 +29,10 @@ class Player(GameSprite):
         keys = key.get_pressed()
         if keys[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[k_s] and self.rect.y < win_height - 80:
+        if keys[K_s] and self.rect.y < win_height - 80:
             self.rect.y += self.speed
-	
-back = (200, 255.255)
+
+back = (200, 255,255)
 win_width = 600
 win_height = 500
 window = display.set_mode((win_width, win_height))
@@ -47,46 +46,43 @@ FPS=60
 racket1 = Player('racket.png', 30,200,4,50,150)
 racket2 = Player('racket.png', 520,200,4,50,150)
 ball = GameSprite('tenis_ball.png',200,200,4,50,50)
+
 font.init()
-font = font.Font('calibri.ttf',35)
+font = font.Font(None,35)
 lose1 = font.render('PLAYER 1 LOSE!', True,(180,0,0))
 lose2 = font.render("PLAYER 2 LOSE!", True, (180,0,0))
-speed_x = 3
-speed_y = 3
-#Zhiharev - 3
+speed_x=3
+speed_y=3
 while game:	
-	for e in event.get():	
-		if e.type == QUIT:
-		game = False
-	    if finish != True:	
-		window.fill(back)
-		racketl.update_l()
-		racket2.update_r()
-		ball.rect.x += speed_x
-		ball.rect.y += speed_y
-		if sprite.collide_rect(racketl, ball) or sprite.collide_rect(racket2, ball):
-		    speed_x *= -1
-		    speed_y *= 1
-#ralenno - 4
+    for e in event.get():	
+        if e.type == QUIT:
+            game = False
+    if finish != True:	
+        window.fill(back)
+        racket1.update_l()
+        racket2.update_r()
+        
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+            speed_y *= 1
+        if ball.rect.y>win_height-50 or ball.rect.y<0:
+            speed_y *= -1
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200, 200))
+            game_over = True
+        if ball.rect.x > win_width:
+            finisht = True
+            window.blit(lose2, (200,200))
+            game_over = True
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
+    display.update()
+    clock.tick(FPS)
 
-
-
-#5
-    if ball.rect.y > win_height-50 or ball.rect.y < 0:
-        speed_y *= -1
-    if ball.rect.x < 0:
-        finish = True
-        window.blit(lose1, (200, 200))
-        game_over = True
-    if ball.rect.x > win_width:
-        finisht = True
-        window.blit(lose2, (200,200))
-        game_over = True
-    racket1.reset()
-    racket2.reset()
-    ball.reset()
-sidplay.update()
-clock.tick(FPS)
 
 
 
