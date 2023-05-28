fd08a70d523a361460a37c4e1e547ef38d9dddf6
Arseniy30506 <134214510+Arseniy30506@users.noreply.github.com>
2023-05-28 17:10:27 +0300

    Update ping_pong.py

--- ping_pong.py
+++ ping_pong.py
@@ -37,7 +37,22 @@ class Player(GameSprite):
 
 #ralenno - 4
 
-#5 
+#5
+    if ball.rect.y > win_height-50 or ball.rect.y < 0:
+        speed_y *= -1
+    if ball.rect.x < 0:
+        finish = True
+        window.blit(lose1, (200, 200))
+        game_over = True
+    if ball.rect.x > win_width:
+        finisht = True
+        window.blit(lose2, (200,200))
+        game_over = True
+    racket1.reset()
+    racket2.reset()
+    ball.reset()
+sidplay.update()
+clock.tick(FPS)
 
 
 
