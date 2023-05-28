f80524b65fd4ceae6b1c2da15023dc9834929b37
Makki1337 <134214428+Makki1337@users.noreply.github.com>
2023-05-28 17:20:43 +0300

    Update ping_pong.py

--- ping_pong.py
+++ ping_pong.py
@@ -49,6 +49,26 @@ while game:
 		    speed_y *= 1
 #ralenno - 4
 
+back = (200, 255.255)
+    win_width = 600
+    win_height = 500
+    window = display.set_mode((win_width, win_height))
+    window.fill(back)
+
+    game=True
+    finish = False
+    clock = time.Clock()
+    FPS=60
+
+    racket1 = Player('racket.png', 30,200,4,50,150)
+    racket2 = Player('racket.png', 520,200,4,50,150)
+    ball = GameSprite('tenis_ball.png',200,200,4,50,50)
+
+    font.init()
+    font = font.Font('calibri.ttf',35)
+    lose1 = font.render('PLAYER 1 LOSE!', True,(180,0,0))
+    lose2 = font.render("PLAYER 2 LOSE!", True, (180,0,0))
+
 #5
     if ball.rect.y > win_height-50 or ball.rect.y < 0:
         speed_y *= -1
