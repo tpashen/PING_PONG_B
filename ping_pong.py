bb78955856a64d70241a7e23825688e6ea7445bb
AkantorMagala <134850002+AkantorMagala@users.noreply.github.com>
2023-05-28 16:51:11 +0300

    Update ping_pong.py
    #ясделал-№1

--- ping_pong.py
+++ ping_pong.py
@@ -1,5 +1,23 @@
 #Sidorenko - 1
+from pygame import*
+'''Необходимые классы'''
 
+#класс-родитель для спрайтов
+class GameSprite(sprite.Sprite):
+    #конструктор класса
+        #конструктор класса
+    def __init__(self, player_image, player_x, player_y, player_speed, width, height):
+        super().__init__()
+
+        self.image = transform.scale(image.load(player_image),(widt, height))
+        self.speed = player_speed
+
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
 #Fedaev - 2
 
 #Zhiharev - 3
