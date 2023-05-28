f229217606f1b546b3b4521f7b25f07638a3b92d
Makki1337 <134214428+Makki1337@users.noreply.github.com>
2023-05-28 17:28:01 +0300

    Update ping_pong.py

--- ping_pong.py
+++ ping_pong.py
@@ -34,24 +34,23 @@ class Player(GameSprite):
             self.rect.y += self.speed
 	
 back = (200, 255.255)
-    win_width = 600
-    win_height = 500
-    window = display.set_mode((win_width, win_height))
-    window.fill(back)
+win_width = 600
+win_height = 500
+window = display.set_mode((win_width, win_height))
+window.fill(back)
 
-    game=True
-    finish = False
-    clock = time.Clock()
-    FPS=60
+game=True
+finish = False
+clock = time.Clock()
+FPS=60
 
-    racket1 = Player('racket.png', 30,200,4,50,150)
-    racket2 = Player('racket.png', 520,200,4,50,150)
-    ball = GameSprite('tenis_ball.png',200,200,4,50,50)
-
-    font.init()
-    font = font.Font('calibri.ttf',35)
-    lose1 = font.render('PLAYER 1 LOSE!', True,(180,0,0))
-    lose2 = font.render("PLAYER 2 LOSE!", True, (180,0,0))
+racket1 = Player('racket.png', 30,200,4,50,150)
+racket2 = Player('racket.png', 520,200,4,50,150)
+ball = GameSprite('tenis_ball.png',200,200,4,50,50)
+font.init()
+font = font.Font('calibri.ttf',35)
+lose1 = font.render('PLAYER 1 LOSE!', True,(180,0,0))
+lose2 = font.render("PLAYER 2 LOSE!", True, (180,0,0))
 
 #Zhiharev - 3
 while game:	
